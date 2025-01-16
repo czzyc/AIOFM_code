d89d8daa41d61ee33c091d78cbf5265cff28c1a4
zzy <zzy15178648783@163.com>
2025-01-16 10:26:36 +0800

    11

---
+++ cameraport.h
@@ -0,0 +1,26 @@
+﻿#ifndef CAMERAPORT_H
+#define CAMERAPORT_H
+
+#include <QWidget>
+
+namespace Ui {
+class CameraPort;
+}
+
+class CameraPort : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CameraPort(QWidget *parent = nullptr);
+    ~CameraPort();
+public:
+    void on_pushButton_clicked();
+private slots:
+    //void on_pushButton_clicked();
+
+private:
+    Ui::CameraPort *ui;
+};
+
+#endif // CAMERAPORT_H
